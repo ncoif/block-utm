f38f9f8edcbe9b993089a4ed1a0cd502ab8839db
ncoif <ncoif@gmx.com>
2018-01-19 01:18:01 +0000

    first try using webrequest, not working

---
+++ background/background.js
@@ -0,0 +1,45 @@
+
+//https://www.theguardian.com/politics/2018/jan/18/taxpayers-to-foot-200bn-bill-for-pfi-contracts-audit-office
+//?utm_source=esp&utm_medium=Email&utm_campaign=GU+Today+main+NEW+H+categories&utm_term=260850&subid=22703671&CMP=EMCNEWEML6619I2
+
+var strippedParams = [ "utm_source", "utm_medium", "utm_campaign", "utm_term"]
+
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
+}
+
+function stripUtm(requestDetails) {
+  console.log("Received requestDetails: " + requestDetails.url);
+
+  var alteredUrl = requestDetails.url;
+  for (var key in strippedParams) {
+    alteredUrl = removeParam(key, alteredUrl);
+  }
+
+  if (alteredUrl !== requestDetails.url) {
+    console.log("Stripped: " + requestDetails.url + " -> " + alteredUrl)
+  }
+  return {
+    redirectUrl: alteredUrl
+  };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  stripUtm,
+  {urls:["<all_urls>"]},
+  ["blocking"]
+);
