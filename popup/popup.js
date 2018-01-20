86b3a622d2dc81c3e75170db541837381d0dc065
ncoif <ncoif@gmx.com>
2018-01-20 17:07:43 +0000

    Storage return a promise, so properly fix option saving

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,12 @@
 "use strict";
 
+// Change the UI to reflect extension enabled/disabled
+function updateEnabledDisabledUI() {
+  browser.storage.local.get("globalEnabled").then(function(option) {
+    document.getElementById('onoffswitch').checked = option.globalEnabled;
+  });
+}
+
 // Toggle extension enabled/disabled status
 function toggleEnabledDisabled() {
   var globalEnabled = document.getElementById('onoffswitch').checked;
@@ -10,5 +17,7 @@ function toggleEnabledDisabled() {
  * Fill in content into the popup on load
  */
 document.addEventListener("DOMContentLoaded", function () {
+  // Set up the enabled/disabled switch
+  updateEnabledDisabledUI();
   document.getElementById('onoffswitch').addEventListener('click', toggleEnabledDisabled);
 });
