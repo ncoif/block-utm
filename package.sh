3e3c47ff02658624730ac1639319ffa51d3d1aed
ncoif <ncoif@gmx.com>
2018-01-21 23:55:53 +0000

    Add package.sh script to package the final archive

---
+++ package.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Publishing_your_WebExtension
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+echo "Cleaning up target directory..."
+rm -Rf $SCRIPTPATH/target
+
+echo "Copying files into the target archive..."
+rsync -r --exclude-from=$SCRIPTPATH/exclude.txt $SCRIPTPATH/. $SCRIPTPATH/target
+
+echo "Compressing images..."
+for file in $SCRIPTPATH/target/icons/*.png; do pngcrush "$file" "${file%.png}-crushed.png" && mv -v "${file%.png}-crushed.png" "$file"; done
+
+echo "Ziping the finale archive..."
+cd $SCRIPTPATH/target && zip -r -FS block_utm.zip . && cd $SCRIPTPATH
+
+echo "Verifing the finale archive..."
+unzip -l $SCRIPTPATH/target/block_utm.zip
